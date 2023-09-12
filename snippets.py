25546e7508579f4e737b7f478b7e10017b1fa230
Wesley Brown <wesleykebrown@yahoo.com>
2023-09-11 20:29:09 -0400

    Assignment Completed

--- snippets.py
+++ snippets.py
@@ -8,7 +8,7 @@ def snippet_1():
     u = 5
     v = 2
 
-    if u * v = 10:
+    if u * v == 10:
         print(f"The product of u ({u}) and v ({v}) is 10")
     else:
         print(f"The product of u ({u}) and v ({v}) is not 10")
@@ -23,10 +23,10 @@ def snippet_2():
     if z < x:
         print("z is less than x")
 
-    elif z > x and z < y
+    elif z > x and z < y:
         print("z is between x and y")
 
-    else
+    else:
         print("z is greater than y")
 
 
@@ -36,16 +36,16 @@ def snippet_3():
 
     a = 1
     b = 1
-    c = (a > b)
+    c = (a == b)
 
-    print(f"The value of c ({c}) is True since a ({a}) is greater than b ({b}).")
+    print(f"The value of c ({c}) is True since a ({a}) is equal to b ({b}).")
     assert(c == True)  # <-- DO NOT EDIT THIS LINE
 
 
 def snippet_4():
     # TODO: Modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
-    d = (5 < 7) or not (8 < 20)
+    d = (5 < 7) and not (8 < 20)
 
     # TODO: Explain how d is set to False in a comment
     assert(d == False)  # <-- DO NOT EDIT THIS LINE
@@ -58,7 +58,7 @@ def snippet_5():
     m = "GOAT"
     n = "goat"
 
-    o = (m == n)
+    o = (m != n)
 
-    print (f"The value of o ({o}) is False since Python is case-sensitive.")
+    print (f"The value of o ({o}) is True since Python is case-sensitive.")
     assert(o == True)  # <-- DO NOT EDIT THIS LINE
